2d72fdad259b06097e1ef0f5a0cacc662b44a583
Tavo López Hernández <eglhtavo@hotmail.com>
2020-12-14 20:34:20 -0600

    Items orderby - tratando de ordenar los items -- add cosas basura

---
+++ xamarinCompraList/xamarinCompraList/xamarinCompraList/Helpers/ItemTemplateSelector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+using xamarinCompraList.Models;
+
+namespace xamarinCompraList.Helpers
+{
+    public class ItemTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate Item_Listo { get; set; }
+        public DataTemplate Item_NoListo { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            return  (item as Item).Listo == true ?  Item_Listo : Item_NoListo;
+        }
+    }
+}
--- xamarinCompraList/xamarinCompraList/xamarinCompraList/Models/compralistContext.cs
+++ xamarinCompraList/xamarinCompraList/xamarinCompraList/Models/compralistContext.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using xamarinCompraList.Helpers;
 
 namespace xamarinCompraList.Models
 {
@@ -18,8 +19,20 @@ namespace xamarinCompraList.Models
         }
 
         public string Username { get; set; }
+        //public List<Item> ListItems_Base { get; set; }
+        //public ObservableCollection<Item> ListItems_Obs { get; set; }
         //public List<Item> ListItems { get; set; }
         //public ObservableCollection<Item> ListItems { get; set; }
+
+        //public SortableObservableCollection<Item> ListItems { get; set; }
+        //private SortableObservableCollection<Item> listItems;
+
+        //public SortableObservableCollection<Item> ListItems
+        //{
+        //    get { return listItems; }
+        //    set { listItems = value; }
+        //}
+
         private ObservableCollection<Item> listItems;
 
         public ObservableCollection<Item> ListItems
@@ -35,20 +48,38 @@ namespace xamarinCompraList.Models
         //    set { listItems = value; Actualizar(); }
         //}
 
+        //private ObservableCollection<Item> listItems;
+
+        //public ObservableCollection<Item> ListItems
+        //{
+        //    //get { return listItems; }
+        //    //set {
+        //    //    listItems = listItems != null ? new ObservableCollection<Item>((from item in ListItems orderby item.Listo, item.Id descending select item).ToList()) : value;
+        //    //}
+
+        //    get 
+        //    { 
+        //        return new ObservableCollection<Item>((from item in listItems orderby item.Listo, item.Id descending select item).ToList()); 
+        //    }
+        //    set { listItems = value; }
+        //}
+
         public compralistContext()
         {
+            //ListItems_Base = new List<Item>
+            //{
+            //    new Item { Id = 1, Listo = false, Nombre = "1kg de Papas", Quienloagrego = "Tavo" },
+            //    new Item { Id = 2, Listo = false, Nombre = "Jamon", Quienloagrego = "Tavo" },
+            //    new Item { Id = 3, Listo = false, Nombre = "Tomates", Quienloagrego = "Tavo" },
+            //    new Item { Id = 4, Listo = true, Nombre = "Harina", Quienloagrego = "Tavo" }
+            //};
             ListItems = new ObservableCollection<Item>
             {
                 new Item { Id = 1, Listo = false, Nombre = "1kg de Papas", Quienloagrego = "Tavo" },
                 new Item { Id = 2, Listo = false, Nombre = "Jamon", Quienloagrego = "Tavo" },
                 new Item { Id = 3, Listo = false, Nombre = "Tomates", Quienloagrego = "Tavo" },
                 new Item { Id = 4, Listo = true, Nombre = "Harina", Quienloagrego = "Tavo" },
-                new Item { Id = 5, Listo = false, Nombre = "1 celular", Quienloagrego = "Tavo" },
-                new Item { Id = 6, Listo = true, Nombre = "1 par de chanclas", Quienloagrego = "Tavo" },
-                new Item { Id = 7, Listo = false, Nombre = "1 carrito", Quienloagrego = "Tavo" },
-                new Item { Id = 8, Listo = true, Nombre = "5 coca cola 2.5lts", Quienloagrego = "Tavo" },
-                new Item { Id = 9, Listo = false, Nombre = "2 paquetes de platos", Quienloagrego = "Tavo" },
-                new Item { Id = 10, Listo = true, Nombre = "1 paquete de vasos", Quienloagrego = "Tavo" }
+                new Item { Id = 5, Listo = false, Nombre = "1 celular", Quienloagrego = "Tavo" }
             };
             //ListItems = (ObservableCollection<Item>)ListItems.OrderBy(x => x.Listo ? 0 : 1);
             //var result = (from item in ListItems orderby item.Listo select item).ToList();
@@ -59,12 +90,19 @@ namespace xamarinCompraList.Models
             //}
             //ListItems = ListItems.OrderBy(x => x.Listo).ToList();
             //ListItems = new ObservableCollection<Item>((from item in ListItems orderby item.Listo select item).ToList());
+            //ListItems = new SortableObservableCollection<Item>();
+            //ListItems.Add(new Item { Id = 1, Listo = false, Nombre = "false 1", Quienloagrego = "Tavo" });
+            //ListItems.Add(new Item { Id = 1, Listo = true, Nombre = "true 1", Quienloagrego = "Tavo" });
+            //ListItems.Add(new Item { Id = 1, Listo = false, Nombre = "false 2", Quienloagrego = "Tavo" });
             List_OrderBy();
         }
 
         public void List_OrderBy()
         {
+            //ListItems.Sort(x => x.Listo, ListSortDirection.Ascending);
             ListItems = new ObservableCollection<Item>((from item in ListItems orderby item.Listo, item.Id descending select item).ToList());
+            //ListItems_Base = ListItems_Base.OrderBy(x => x.Listo).ThenBy(x => x.Nombre).ToList();
+            //ListItems_Obs = new ObservableCollection<Item>(ListItems_Base);
         }
 
         public void Items_LoadAll()
@@ -88,12 +126,22 @@ namespace xamarinCompraList.Models
                 Nombre = itemcontent,
                 Quienloagrego = username
             });
+
+            //int result = ListItems_Obs.Count > 0 ? ListItems_Obs.OrderByDescending(x => x.Id).FirstOrDefault().Id : 1;
+            //ListItems_Base.Add(new Item
+            //{
+            //    Id = result + 1,
+            //    Listo = false,
+            //    Nombre = itemcontent,
+            //    Quienloagrego = username
+            //});
             List_OrderBy();
         }
 
         public void Item_Delete(Item item)
         {
             ListItems.Remove(item);
+            //ListItems_Base.Remove(item);
             //List_OrderBy();
         }
 
@@ -101,6 +149,10 @@ namespace xamarinCompraList.Models
         {
             ListItems.FirstOrDefault(x => x.Id == item.Id).Listo = !item.Listo;
             ListItems.FirstOrDefault(x => x.Id == item.Id).Quienloagrego = username;
+
+            //ListItems_Base.FirstOrDefault(x => x.Id == item.Id).Listo = !item.Listo;
+            //ListItems_Base.FirstOrDefault(x => x.Id == item.Id).Quienloagrego = username;
+
             List_OrderBy();
             //ListItems = ListItems;
         }
